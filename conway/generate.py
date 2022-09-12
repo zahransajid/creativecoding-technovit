30367b776f0157ed779c30dc4934468445ba9473
zahransajid <zahransajid3@gmail.com>
2022-09-12 16:42:45 +0530

    Added documentation into code, generated obj of conway for 5 generations

--- conway/generate.py
+++ conway/generate.py
@@ -1,39 +1,23 @@
 import sys
 import os
 import bpy
-import numpy as np
-import time
+
 Space = bpy.data.texts["Space"].as_module()
 
-def main() -> None:
-#    RULESET: str = "B3/S23"
-#    Life = Space.Space(5, 5, 4, chance_1_divided_by=4)
-#    Life.set_rules(RULESET)
-#    Life.randomize()
-#    state = Life.export()
-#    print(state.shape)
-    mat = bpy.data.materials['Material.002']
-    solver = Solver()
-    print(solver.shape)
-    for i in range(9):
-        solver.realize()
-        solver.update()
-        select_metaballs()
-        obj = mesh_metaballs()
-        obj.material_slot_add()
-#        obj.data.materials[0] = mat
-    user_data = bpy.data.user_map()
-    meshes = []
-    for mesh in bpy.data.meshes.keys():
-        mesh = bpy.data.meshes[mesh]
-        if('Mesh' in mesh.name):
-            if(len(user_data[mesh]) > 0):
-                mesh.materials[0] = mat
-                print(f"Changed mat for {mesh.name}")
-                meshes.append(mesh)
 
-class Solver():
-    def __init__(self):
+def main() -> None:
+    """Main function, initializes solver and creates cubes for 5 iterations of Conways game of life"""
+    solver = Solver()
+    x_offset_scale = 5
+    for i in range(10):
+        solver.realize(i * x_offset_scale)
+        solver.update()
+
+
+class Solver:
+    """Instantiates a Space object with rules and dimensions for 3d cellular automata"""
+
+    def __init__(self) -> None:
         self.RULESET = "B3/S23"
         self.Life = Space.Space(5, 5, 5, chance_1_divided_by=4)
         self.Life.set_rules(self.RULESET)
@@ -41,36 +25,38 @@ class Solver():
         self.Life.update()
         self.state = self.Life.export()
         self.shape = self.state.shape
-    def map_to_space(self,dims):
-        pass
+
     def update(self):
+        """Updates the state variable:
+        - state is a 3D numpy array of shape (5,5,5) ) (can be changed)
+        - calling this updates the array to the next generation"""
         self.Life.update()
         self.state = self.Life.export()
 
-    def create_ball(self,pos):
-        bpy.ops.object.metaball_add(type='BALL', enter_editmode=False, align='WORLD', location=pos, scale=(1, 1, 1))
+    def create_cube(self, pos: tuple, x_offset: int):
+        """Creates a cube at the given point with an offset of x units from the centre.
+        Inputs are:
+        - pos : a tuple of (x,y,z) coordinates
+        - x_offset : int value mentioning how many units to offset from the centre"""
+        pos[1] += x_offset
+        bpy.ops.mesh.primitive_cube_add(
+            size=1, enter_editmode=False, align="WORLD", location=pos, scale=(1, 1, 1)
+        )
 
-    def realize(self):
+    def realize(self, x_offset: int):
+        """Creates cubes denoting live cells in the 3D cellular automata.
+        - x_offset : int value mentioning how many units to offset from the centre"""
         xs = [x for x in range(self.shape[0])]
         ys = [x for x in range(self.shape[1])]
         zs = [x for x in range(self.shape[2])]
         for x in xs:
             for y in ys:
                 for z in zs:
-                    pos = [x,y,z]
-                    if(self.state[pos[0],pos[1],pos[2]] != 0):
-                        print(f"yes, {pos}")
-                        self.create_ball(pos)
+                    pos = [x, y, z]
+                    if self.state[pos[0], pos[1], pos[2]] != 0:
+                        print(f"created cube at, {pos}")
+                        self.create_cube(pos, x_offset)
 
-def select_metaballs():
-    bpy.ops.object.select_grouped(type='TYPE')
 
-def mesh_metaballs():
-    bpy.ops.object.convert(target='MESH')
-    return bpy.ops.object
-
-def delete_mesh(object):
-    object.delete()
-    
-
+# call the main function
 main()
